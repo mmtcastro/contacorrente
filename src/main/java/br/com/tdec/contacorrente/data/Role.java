875b35a2b65f77c72f680fcf3621e378440acc49
start.vaadin.com <no-reply@vaadin.com>
2023-03-09 01:25:42 +0000

    Generated project

---
+++ src/main/java/br/com/tdec/contacorrente/data/Role.java
@@ -0,0 +1,5 @@
+package br.com.tdec.contacorrente.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/br/com/tdec/contacorrente/security/AuthenticatedUser.java
@@ -0,0 +1,30 @@
+package br.com.tdec.contacorrente.security;
+
+import br.com.tdec.contacorrente.data.entity.User;
+import br.com.tdec.contacorrente.data.service.UserRepository;
+import com.vaadin.flow.spring.security.AuthenticationContext;
+import java.util.Optional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticatedUser {
+
+    private final UserRepository userRepository;
+    private final AuthenticationContext authenticationContext;
+
+    public AuthenticatedUser(AuthenticationContext authenticationContext, UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.authenticationContext = authenticationContext;
+    }
+
+    public Optional<User> get() {
+        return authenticationContext.getAuthenticatedUser(UserDetails.class)
+                .map(userDetails -> userRepository.findByUsername(userDetails.getUsername()));
+    }
+
+    public void logout() {
+        authenticationContext.logout();
+    }
+
+}
